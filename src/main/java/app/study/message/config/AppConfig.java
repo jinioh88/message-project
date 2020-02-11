a34785889c3d6c7342fdba3d8d691638145a5cb9
jinio <jinioh88@gmail.com>
2020-02-12 01:14:22 +0900

    fix: filter 추가

--- src/main/java/app/study/message/config/AppConfig.java
+++ src/main/java/app/study/message/config/AppConfig.java
@@ -1,13 +1,24 @@
 package app.study.message.config;
 
-import app.study.message.repository.MessageRepository;
-import app.study.message.service.MessageService;
+import app.study.message.filter.AuditingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
 @ComponentScan("app.study")
 public class AppConfig {
+    @Bean
+    public FilterRegistrationBean<AuditingFilter> auditingFilterFilterRegistrationBean() {
+        FilterRegistrationBean<AuditingFilter> registration = new FilterRegistrationBean<>();
+        AuditingFilter filter = new AuditingFilter();
+        registration.setFilter(filter);
+        registration.setOrder(Integer.MAX_VALUE);
+        registration.setUrlPatterns(Arrays.asList("/messages/*"));  // Filter를 등록할 경로
 
+        return registration;
+    }
 }
